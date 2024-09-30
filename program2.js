6e932535c0aa06dc0ea38a45226e8b62a037b3c7
BishakhaGupta <guptabishakha2003@gmail.com>
2024-09-30 14:14:31 +0530

    Auto commit on save

--- program2.js
+++ program2.js
@@ -3,8 +3,36 @@
  * @return {number}
  */
 var romanToInt = function(s) {
-    
+    const romanMap = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    };
+
+    let total = 0;
+    const length = s.length;
+
+    for (let i = 0; i < length; i++) {
+        const value = romanMap[s[i]];
+        const nextValue = romanMap[s[i + 1]];
+
+        // If the current value is less than the next value, subtract it
+        if (nextValue && value < nextValue) {
+            total -= value;
+        } else {
+            total += value;
+        }
+    }
+
+    return total;
 };
 
+console.log(romanToInt("III"));      
+console.log(romanToInt("LVIII"));    
+console.log(romanToInt("MCMXCIV"));  
 
-module.exports={romanToInt}
+module.exports = { romanToInt };
