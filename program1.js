d25cd29b754954f5dcaa7f2d9a84492ebc1b6d6a
BishakhaGupta <guptabishakha2003@gmail.com>
2024-09-30 13:58:33 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -11,32 +11,3 @@ module.exports = { isValid };
 
 
 
-
-  @param {string} s
-  @return {boolean}
- */
-var isValid = function(s) {
-    const stack = [];
-    const map = {
-        '(': ')',
-        '{': '}',
-        '[': ']'
-    };
-
-    for (const char of s) {
-        if (map[char]) {
-            // If it's an opening bracket, push the corresponding closing bracket onto the stack
-            stack.push(map[char]);
-        } else {
-            // If it's a closing bracket, check if it matches the last opened one
-            if (stack.pop() !== char) {
-                return false;
-            }
-        }
-    }
-
-    // If the stack is empty, all brackets are matched
-    return stack.length === 0;
-};
-
-module.exports = { isValid };
