299a22f0878c530a093dc24383e53f9ea936dbb0
BishakhaGupta <guptabishakha2003@gmail.com>
2024-09-30 13:58:43 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -12,7 +12,7 @@ var isValid = function(s) {
 
     for (const char of s) {
         if (map[char]) {
-            // If it's an opening bracket, push the corresponding closing bracket onto the stack
+            
             stack.push(map[char]);
         } else {
             // If it's a closing bracket, check if it matches the last opened one
