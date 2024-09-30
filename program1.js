ba5e19b92a7e5d4d5a63399b6ced29d473da8864
BishakhaGupta <guptabishakha2003@gmail.com>
2024-09-30 14:02:14 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -12,23 +12,18 @@ var isValid = function(s) {
 
     for (const char of s) {
         if (map[char]) {
-            
-            stack.push(map[char]);
+            // If it's an opening bracket, push the corresponding closing bracket onto the stack
+            stack.push(char); // Push the opening bracket instead of the closing one
         } else {
-            
-            if (stack.pop() !== char) {
+            // If it's a closing bracket, check if it matches the last opened one
+            if (stack.length === 0 || map[stack.pop()] !== char) {
                 return false;
             }
         }
     }
 
-
+    // If the stack is empty, all brackets are matched
     return stack.length === 0;
 };
 
 module.exports = { isValid };
-
-
-
-
-
